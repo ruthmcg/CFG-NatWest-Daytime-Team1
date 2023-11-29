b280c87300bcf86fddcc33071148333d0b9a08b5
Caity <110776833+Catreeney2@users.noreply.github.com>
2023-11-29 15:45:41 +0000

    Update App.js
    Caity edits for hw2

--- homework-2/src/App.js
+++ homework-2/src/App.js
@@ -12,6 +12,12 @@ function App() {
           <li>My favourite hobby is hillwalking ⛰️</li>
           <li>I enjoy it because it gets me out in the fresh air, and it's a good opportunity to chat with my sons while we explore 👩‍👦‍👦</li>
         </ul>
+
+            <h2 id="caityHeading"> Caity </h2>
+        <ul>
+          <li>My favourite hobby is playing fiddle 🎻</li>
+          <li> I love it because I get to make great music with friends and learn about other cultures, as well as travelling the world! </li>
+        </ul>
       </div>
 
       </header>
