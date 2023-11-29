5a4a8c77091537c616eb719d91c8e79dc2470e24
Catreeney2 <c.m.oddy@hotmail.com>
2023-11-29 17:49:19 +0000

    CSS changes - Caity

--- homework-2/src/App.js
+++ homework-2/src/App.js
@@ -12,13 +12,15 @@ function App() {
           <li>My favourite hobby is hillwalking ⛰️</li>
           <li>I enjoy it because it gets me out in the fresh air, and it's a good opportunity to chat with my sons while we explore 👩‍👦‍👦</li>
         </ul>
+        </div>
 
-            <h2 id="caityHeading"> Caity </h2>
+       <div id = 'caityDiv'>
+        <h2 id="caityHeading"> Caity </h2>
         <ul>
           <li>My favourite hobby is playing fiddle 🎻</li>
           <li> I love it because I get to make great music with friends and learn about other cultures, as well as travelling the world! </li>
         </ul>
-      </div>
+        </div> 
 
       </header>
     </div>
