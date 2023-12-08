9aa94a62890e2a4e66bd638d36533eeeeea040d1
16tales <128521409+16tales@users.noreply.github.com>
2023-12-08 10:37:23 +0000

    reduced number of questions

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -17,41 +17,19 @@ const Quiz = () => {
         answerOptions: ['Intermission', 'Mid-life onset', 'Perimenopause'],
         correctAnswer: 'Perimenopause',
     },
-    {
-        questionText: "Which menopause stage has the most intense and disruptive symptoms?",
-        answerOptions: ['Pre-menopause', 'Perimenopause', 'Postmenopause'],
-        correctAnswer: 'Perimenopause',
-    },
+  
     {
         questionText: "Hot flashes that occur with perspiration during sleep are known as:",
         answerOptions: ['Night sweats', 'Morning flashes', 'Evening flushes'],
         correctAnswer: 'Night sweats',
     },
-    {
-        questionText: "Vaginal dryness and discomfort is a common menopausal symptom related to:",
-        answerOptions: ['Hormone changes', 'Poor nutrition', 'Lack of sleep'],
-        correctAnswer: 'Hormone changes',
-    },
-    {
-        questionText: "Which term refers to having no menstrual periods for 12 months?",
-        answerOptions: ['Menopause', 'Perimenopause', 'Postmenopause'],
-        correctAnswer: 'Menopause',
-    },
+ 
     {
         questionText: "Bone loss accelerates during menopause because of:",
         answerOptions: ['Inflammation', 'Lack of vitamin D', 'Drop in estrogen'],
         correctAnswer: 'Drop in estrogen',
     },
-    {
-        questionText: "Emotional changes like mood swings may occur during which stage?",
-        answerOptions: ['Perimenopause', 'Postmenopause', 'Menopause'],
-        correctAnswer: 'Perimenopause',
-    },
-    {
-        questionText: "Who is most at risk for weight gain and slower metabolism during menopause?",
-        answerOptions: ['Women with low fitness', 'Women with previous pregnancies', 'All women'],
-        correctAnswer: 'All women',
-    },
+ 
     {
         questionText: "Menopause symptoms may persist for how long?",
         answerOptions: ['6 months to a year ', 'Up to 2-5 years', "Throughout a woman's life"],
