1dbe1fb9c76815e559efbb5c017aa024792a1357
16tales <128521409+16tales@users.noreply.github.com>
2023-12-12 15:01:26 +0000

    New button added

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -13,27 +13,27 @@ const Quiz = () => {
       correctAnswer: 'Late 40s to early 50s',
     },
     {
-        questionText: "In the years leading up to menopause, what is the term for irregular cycles and menopause symptoms?",
-        answerOptions: ['Intermission', 'Mid-life onset', 'Perimenopause'],
-        correctAnswer: 'Perimenopause',
+      questionText: "In the years leading up to menopause, what is the term for irregular cycles and menopause symptoms?",
+      answerOptions: ['Intermission', 'Mid-life onset', 'Perimenopause'],
+      correctAnswer: 'Perimenopause',
     },
   
     {
-        questionText: "Hot flashes that occur with perspiration during sleep are known as:",
-        answerOptions: ['Night sweats', 'Morning flashes', 'Evening flushes'],
-        correctAnswer: 'Night sweats',
+      questionText: "Hot flashes that occur with perspiration during sleep are known as:",
+      answerOptions: ['Night sweats', 'Morning flashes', 'Evening flushes'],
+      correctAnswer: 'Night sweats',
     },
  
     {
-        questionText: "Bone loss accelerates during menopause because of:",
-        answerOptions: ['Inflammation', 'Lack of vitamin D', 'Drop in estrogen'],
-        correctAnswer: 'Drop in estrogen',
+      questionText: "Bone loss accelerates during menopause because of:",
+      answerOptions: ['Inflammation', 'Lack of vitamin D', 'Drop in estrogen'],
+      correctAnswer: 'Drop in estrogen',
     },
  
     {
-        questionText: "Menopause symptoms may persist for how long?",
-        answerOptions: ['6 months to a year ', 'Up to 2-5 years', "Throughout a woman's life"],
-        correctAnswer: 'Up to 2-5 years',
+      questionText: "Menopause symptoms may persist for how long?",
+      answerOptions: ['6 months to a year ', 'Up to 2-5 years', "Throughout a woman's life"],
+      correctAnswer: 'Up to 2-5 years',
     },
 
 
